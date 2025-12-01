49b2ddc4874b9ebf71b0ac5f0294b3d00f232fc3
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-01 21:04:17 +0100

    Cliente.Componente01.v1.0

---
+++ src/Cliente/Componente01/src/main/java/org/Componente01/Persistencia/ClienteAPI.java
@@ -0,0 +1,14 @@
+package Persistencia;
+
+public class ClienteAPI {
+
+	/**
+	 * 
+	 * @param token
+	 */
+	public Boolean enviarTokenAlServidor(String token) {
+		// TODO - implement ClienteAPI.enviarTokenAlServidor
+		throw new UnsupportedOperationException();
+	}
+
+}
