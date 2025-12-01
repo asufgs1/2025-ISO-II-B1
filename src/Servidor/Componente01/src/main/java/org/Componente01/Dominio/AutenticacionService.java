7ca08d949c67c7e842b5923156264ad89ae9181d
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-01 21:03:13 +0100

    Servidor.Componente01.v1.0

---
+++ src/Servidor/Componente01/src/main/java/org/Componente01/Dominio/AutenticacionService.java
@@ -0,0 +1,19 @@
+package Dominio;
+
+public class AutenticacionService {
+
+	/**
+	 * 
+	 * @param token
+	 */
+	public Usuario verificarToken(String token) {
+		// TODO - implement AutenticacionService.verificarToken
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement AutenticacionService.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Servidor/Componente01/src/main/java/org/Componente01/Persistencia/SSO_UCLM_Cliente.java
@@ -0,0 +1,21 @@
+package Persistencia;
+
+import Dominio.*;
+
+public class SSO_UCLM_Cliente {
+
+	/**
+	 * 
+	 * @param token
+	 */
+	public Usuario validarToken(String token) {
+		// TODO - implement SSO_UCLM_Cliente.validarToken
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement SSO_UCLM_Cliente.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
