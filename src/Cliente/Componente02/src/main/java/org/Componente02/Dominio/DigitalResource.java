595dd656bb48e015325c4f03e5a147ac73c7d18a
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-04 19:16:37 +0100

    Componente02-Cliente (Implementación) Refs #24

---
+++ src/Cliente/Componente02/src/main/java/org/Componente02/Dominio/DigitalResource.java
@@ -0,0 +1,8 @@
+package org.Componente02.Dominio;
+
+public class DigitalResource extends CatalogItem {
+
+	private String url;
+	private List<String> derechosAcceso;
+
+}
---
+++ src/Cliente/Componente02/src/main/java/org/Componente02/Presentacion/LoguinUI.java
@@ -0,0 +1,15 @@
+package org.Componente02.Presentacion;
+
+public class LoguinUI {
+
+	public void mostrarLogin() {
+		// TODO - implement LoguinUI.mostrarLogin
+		throw new UnsupportedOperationException();
+	}
+
+	public void iniciarSSO() {
+		// TODO - implement LoguinUI.iniciarSSO
+		throw new UnsupportedOperationException();
+	}
+
+}
