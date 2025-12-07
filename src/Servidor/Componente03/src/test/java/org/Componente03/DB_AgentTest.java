12d68c81dc72b5af11b9ac48e6a2e2c9c9cd087c
Adrián <adrian.diaz10@alu.uclm.es>
2025-12-07 18:00:33 +0100

    Componente03 - Servidor (Pruebas). Refs #37

---
+++ src/Servidor/Componente03/src/test/java/org/Componente03/DB_AgentTest.java
@@ -0,0 +1,19 @@
+package org.Componente03;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DB_AgentTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
