e4ea351e5b73b49067d3380252328d7cb41e600f
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-07 18:51:54 +0100

    Componente03-Cliente (Implementacion) Refs #43

---
+++ src/Cliente/Componente03/src/main/java/org/Componente03/Dominio/DocumentoDAO.java
@@ -0,0 +1,23 @@
+package Dominio;
+
+public class DocumentoDAO {
+
+	/**
+	 * 
+	 * @param idDocumento
+	 */
+	public Documento obtenerDocumento(int idDocumento) {
+		// TODO - implement DocumentoDAO.obtenerDocumento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param documento
+	 */
+	public boolean guardarDocumento(Documento documento) {
+		// TODO - implement DocumentoDAO.guardarDocumento
+		throw new UnsupportedOperationException();
+	}
+
+}
