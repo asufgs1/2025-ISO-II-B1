a736e21dfbdaa78e172b1021352cc6b07f1ca86b
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-07 20:07:17 +0100

    Componente04-Cliente (Implementacion) Refs #49

---
+++ src/Cliente/Componente04/src/main/java/org/Componente04/Presentación/IUBusquedas.java
@@ -0,0 +1,18 @@
+package Presentación;
+
+import java.util.List;
+
+import Dominio.*;
+
+public class IUBusquedas {
+
+	/**
+	 * 
+	 * @param filtros
+	 */
+	public List<Documentos> realizarBusquedaFiltrada(filtros filtros) {
+		// TODO - implement IUBusquedas.realizarBusquedaFiltrada
+		throw new UnsupportedOperationException();
+	}
+
+}
