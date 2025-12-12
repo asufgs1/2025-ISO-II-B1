2487cb7282dcca7eb4788766ec1e119df92966d6
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-12 19:37:52 +0100

    Componente08-Cliente (Implementacion) Refs #73

---
+++ src/Cliente/Componente08/src/main/java/org/Componente08/Persistencia/DB_Agent.java
@@ -0,0 +1,19 @@
+package Persistencia;
+
+public class DB_Agent {
+
+	public void connect() {
+		// TODO - implement DB_Agent.connect
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int update(String SQL) {
+		// TODO - implement DB_Agent.update
+		throw new UnsupportedOperationException();
+	}
+
+}
