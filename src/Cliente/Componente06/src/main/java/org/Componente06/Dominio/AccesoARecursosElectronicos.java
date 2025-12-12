95343835ae6175742ba2e263bc7dba29cf410388
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-12 18:29:56 +0100

    Componente06-Cliente (Implementacion) Refs #61

---
+++ src/Cliente/Componente06/src/main/java/org/Componente06/Dominio/AccesoARecursosElectronicos.java
@@ -0,0 +1,10 @@
+package Dominio;
+
+public class AccesoARecursosElectronicos {
+
+	public Dominio.Recursos[] ConsultarRecursos() {
+		// TODO - implement AccesoARecursosElectronicos.ConsultarRecursos
+		throw new UnsupportedOperationException();
+	}
+
+}
