d1cb67e99cc40ff821047a3a7ae97cce0e20739c
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-15 19:29:24 +0100

    Componente08-Servidor (Implementacion) Refs #118

---
+++ src/Servidor/Componente08/src/main/java/org/Componente08/Presentacion/AuditEventDTO.java
@@ -0,0 +1,13 @@
+public class AuditEventDTO {
+
+	private String id;
+	private String timestamp;
+	private String actorUserID;
+	private String actionRole;
+	private String action;
+	private String resourceType;
+	private String resourceId;
+	private String result;
+	private String ip;
+
+}
