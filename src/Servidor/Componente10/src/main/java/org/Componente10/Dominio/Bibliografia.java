fb9ac23c32b82d25377fe746a7c55065c2e11403
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-16 19:55:29 +0100

    Componente10-Servidor (Implementacion) Refs #130

---
+++ src/Servidor/Componente10/src/main/java/org/Componente10/Dominio/Bibliografia.java
@@ -0,0 +1,15 @@
+package Dominio;
+
+public class Bibliografia {
+
+	private int IdBibliografia;
+	private String Documento;
+	private String Estado;
+	private int Fecha;
+
+	public void toString() {
+		// TODO - implement Bibliografia.toString
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Servidor/Componente10/src/main/java/org/Componente10/Dominio/BibliografiaDAO.java
@@ -0,0 +1,28 @@
+package Dominio;
+
+public class BibliografiaDAO {
+
+	/**
+	 * 
+	 * @param idBibliografia
+	 */
+	public Bibliografia obtenerBibliografia(int idBibliografia) {
+		// TODO - implement BibliografiaDAO.obtenerBibliografia
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param bibliografia
+	 */
+	public boolean guardarBibliografia(Bibliografia bibliografia) {
+		// TODO - implement BibliografiaDAO.guardarBibliografia
+		throw new UnsupportedOperationException();
+	}
+
+	public Dominio.Hardware MostarListaHardware() {
+		// TODO - implement BibliografiaDAO.MostarListaHardware
+		throw new UnsupportedOperationException();
+	}
+
+}
