1728d135b6489b1578f6652f516dac990f323b76
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-16 22:01:35 +0100

    Componente09-Cliente (Implementacion) Refs #143

---
+++ src/Cliente/Componente09/src/main/java/org/Componente09/Presentación/IUParametrosSistema.java
@@ -0,0 +1,19 @@
+package Presentación;
+
+public class IUParametrosSistema {
+
+	public list<Parametros> consultarParametrosSistema() {
+		// TODO - implement IUParametrosSistema.consultarParametrosSistema
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Parametro
+	 */
+	public void modificarParametroSistema(int Parametro) {
+		// TODO - implement IUParametrosSistema.modificarParametroSistema
+		throw new UnsupportedOperationException();
+	}
+
+}
