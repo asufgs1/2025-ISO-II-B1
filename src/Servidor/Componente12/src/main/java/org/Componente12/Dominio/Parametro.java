5f108675fac3020df3d75369bad4ba210e0f3ff0
VictorCons <Victor.Cerrillo@alu.uclm.es>
2025-12-16 22:00:18 +0100

    Componente12-Servidor (Implementacion) Refs #143

---
+++ src/Servidor/Componente12/src/main/java/org/Componente12/Dominio/Parametro.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class Parametro {
+
+	private int IdParametro;
+	private String tipoParametro;
+	private String contenido;
+
+	public void toString() {
+		// TODO - implement Parametro.toString
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Servidor/Componente12/src/main/java/org/Componente12/Persistencia/DBAgent.java
@@ -0,0 +1,55 @@
+package Persistencia;
+
+public class DBAgent {
+
+	private Conection mDB;
+	private String url;
+	private String driver;
+
+	public void getAgent() {
+		// TODO - implement DBAgent.getAgent
+		throw new UnsupportedOperationException();
+	}
+
+	public void connect() {
+		// TODO - implement DBAgent.connect
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int insert(String SQL) {
+		// TODO - implement DBAgent.insert
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int delete(String SQL) {
+		// TODO - implement DBAgent.delete
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int update(String SQL) {
+		// TODO - implement DBAgent.update
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SQL
+	 */
+	public int select(String SQL) {
+		// TODO - implement DBAgent.select
+		throw new UnsupportedOperationException();
+	}
+
+}
